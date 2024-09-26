c83f7b1b747b04ebde606168cc7d5e961df670e6
jperey <jamjam.perey@gmail.com>
2024-09-26 06:59:26 -0700

    created functions for pc choice and player choice

--- rps.js
+++ rps.js
@@ -1,2 +1,14 @@
 console.log("Welcome to the Jammin Rock Paper Scissors (RPS) game!");
 console.log("can you see this?")
+
+function getComputerChoice(){
+    compChoice = Math.floor(Math.random() * 3);
+    return compChoice;
+}
+
+function getPlayerChoice(){
+    playerChoice = Number(prompt("Please input 0, 1, or 2. 0: Rock , 1: Paper, 2: Scissors - "))
+    return playerChoice;
+}
+
+let pinput = getPlayerChoice()
